e1b065a15446b789d479aac624cc13889c76a9d5
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-10-13 19:33:02 -0300

    feat: implementing UserRules class to validation and apllying Factory pattern on User entity to validate it

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,11 +1,18 @@
 import { Entity } from "@/shared/domain/entities/Entity";
 import { UserProps } from "../types/user.types";
+import { UserValidatorFactory } from "../validators/user.validator";
 
 export class UserEntity extends Entity<UserProps>{
 
     constructor(public readonly props: UserProps, id?: string) {
+        UserEntity.validate(props);
         super(props, id)
         this.props.createdAt = this.props.createdAt ?? new Date();
+    }
+
+    static validate(props: UserProps) {
+        const validator = UserValidatorFactory.create();
+        validator.validate(props);
     }
 
     get name() {
@@ -33,10 +40,12 @@ export class UserEntity extends Entity<UserProps>{
     }
 
     update(value: string): void {
+        UserEntity.validate({ ...this.props, name: value });
         this.name = value;
     }
 
     updatePassword(value: string): void {
+        UserEntity.validate({ ...this.props, password: value });
         this.password = value;
     }
 }
---
+++ src/user/domain/validators/__tests__/unit/user.validator.spec.ts
@@ -0,0 +1,33 @@
+import { UserValidator, UserValidatorFactory } from "../../user.validator";
+
+describe("Validating User with UserValidator", () => {
+    let SUT: UserValidator;
+
+    beforeEach(() => {
+        SUT = UserValidatorFactory.create();
+    });
+
+    describe("Validating name property", () => {
+        it("", () => {
+
+        });
+    });
+
+    describe("Validating email property", () => {
+        it("", () => {
+
+        });
+    });
+
+    describe("Validating password property", () => {
+        it("", () => {
+
+        });
+    });
+
+    describe("Validating createdAt property", () => {
+        it("", () => {
+
+        });
+    });
+});
