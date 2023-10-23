85656c4d93bde7337c6a2fa518f8d22d3eb6e259
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-10-23 20:58:44 -0300

    feat: implementing user in-memory repository

---
+++ src/user/infra/database/in-memory/user.in-memory.repository.ts
@@ -0,0 +1,24 @@
+import { ConflictError } from "@/shared/domain/errors/ConflictError";
+import { NotFoundError } from "@/shared/domain/errors/NotFoundError";
+import { InMemoryRepository } from "@/shared/domain/repositories/inMemory.repository";
+import { UserEntity } from "@/user/domain/entities/user.entity";
+import { IUserRepository } from "@/user/domain/repositories/user.repository.contracts";
+
+export class UserInMemoryRepository extends InMemoryRepository<UserEntity> implements IUserRepository {
+    async findByEmail(email: string): Promise<UserEntity> {
+        const _email = `${email}`;
+
+        const entity = this.items.find(item => item.email === _email);
+        if (!entity) throw new NotFoundError(`Entity not found using email ${_email}!`);
+
+        return entity;
+    }
+
+    async emailExists(email: string): Promise<void> {
+        const _email = `${email}`;
+
+        const entity = this.items.find(item => item.email === _email);
+        if (entity) throw new ConflictError("Email address already in use!");
+    }
+
+}
