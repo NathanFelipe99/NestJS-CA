50107ade75a3987d045537e22e7602188f30f7c3
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-10-25 21:22:39 -0300

    feat & tests: implementing searchable repository contracts for custom searches and testing with unit tests

---
+++ src/shared/domain/repositories/__tests__/searchable-repository.contracts.spec.ts
@@ -0,0 +1,145 @@
+import { SearchParams } from "../searchable-repository.contracts";
+
+describe("Searchable repository unit tests", () => {
+    describe("SearchParams tests", () => {
+
+        it("Page prop single test", () => {
+            const SUT = new SearchParams();
+            expect(SUT.page).toEqual(1);
+        });
+
+        it("Page prop batch test", () => {
+            const params = [
+                { page: null, expected: 1 },
+                { page: undefined, expected: 1 },
+                { page: "", expected: 1 },
+                { page: 0, expected: 1 },
+                { page: -1, expected: 1 },
+                { page: 5.5, expected: 1 },
+                { page: true, expected: 1 },
+                { page: {}, expected: 1 },
+                { page: 2, expected: 2 },
+            ];
+
+            params.forEach((param) => {
+                const receivedProps = {
+                    page: param.page as any
+                };
+
+                expect(new SearchParams(receivedProps).page).toEqual(param.expected);
+            });
+        });
+
+        it("PerPage prop single test", () => {
+            const SUT = new SearchParams();
+            expect(SUT.perPage).toEqual(15);
+        });
+
+        it("PerPage prop batch test", () => {
+            const params = [
+                { perPage: null, expected: 15 },
+                { perPage: undefined, expected: 15 },
+                { perPage: "", expected: 15 },
+                { perPage: 0, expected: 15 },
+                { perPage: -1, expected: 15 },
+                { perPage: 5.5, expected: 15 },
+                { perPage: true, expected: 15 },
+                { perPage: {}, expected: 15 },
+                { perPage: 2, expected: 2 },
+            ];
+
+            params.forEach((param) => {
+                const receivedProps = {
+                    perPage: param.perPage as any
+                };
+
+                expect(new SearchParams(receivedProps).perPage).toEqual(param.expected);
+            });
+        });
+
+        it("SortField prop single test", () => {
+            const SUT = new SearchParams();
+            expect(SUT.sortField).toBeNull();
+        });
+        
+        it("SortField prop batch test", () => {
+            const params = [
+                { sortField: null, expected: null },
+                { sortField: undefined, expected: null },
+                { sortField: "", expected: null },
+                { sortField: 0, expected: null },
+                { sortField: -1, expected: "-1" },
+                { sortField: 5.5, expected: "5.5" },
+                { sortField: true, expected: null },
+                { sortField: {}, expected: null },
+                { sortField: "name", expected: "name"}
+            ];
+
+            params.forEach((param) => {
+                const receivedProps = {
+                    sortField: param.sortField as any
+                };
+
+                expect(new SearchParams(receivedProps).sortField).toEqual(param.expected);
+            });
+        });
+
+        it("SortDir prop single test", () => {
+            const SUT = new SearchParams();
+            expect(SUT.sortDir).toBeNull();
+        });
+
+        it("SortDir prop batch test", () => {
+            const params = [
+                { sortDir: null, expected: null },
+                { sortDir: undefined, expected: null },
+                { sortDir: "", expected: null },
+                { sortDir: 0, expected: null },
+                { sortDir: -1, expected: "ASC" },
+                { sortDir: 5.5, expected: "ASC" },
+                { sortDir: true, expected: null },
+                { sortDir: false, expected: null },
+                { sortDir: {}, expected: null },
+                { sortDir: "test", expected: "ASC" },
+                { sortDir: "DESC", expected: "DESC" }
+            ];
+
+            params.forEach((param) => {
+                const receivedProps = {
+                    sortField: "name",
+                    sortDir: param.sortDir as any
+                };
+
+                expect(new SearchParams(receivedProps).sortDir).toEqual(param.expected);
+            });
+        });
+
+        it("Filter prop single test", () => {
+            const SUT = new SearchParams();
+            expect(SUT.filter).toBeNull();
+        });
+
+        it("Filter prop batch test", () => {
+            const params = [
+                { filter: null, expected: null },
+                { filter: undefined, expected: null },
+                { filter: "", expected: null },
+                { filter: 0, expected: null },
+                { filter: -1, expected: "-1" },
+                { filter: 5.5, expected: "5.5" },
+                { filter: true, expected: null },
+                { filter: false, expected: null },
+                { filter: {}, expected: null },
+                { filter: "test", expected: "test" },
+            ];
+
+            params.forEach((param) => {
+                const receivedProps = {
+                    filter: param.filter as any
+                };
+
+                expect(new SearchParams(receivedProps).filter).toEqual(param.expected);
+            });
+        });
+    });
+});
--- src/shared/domain/repositories/searchable-repository.contracts.ts
+++ src/shared/domain/repositories/searchable-repository.contracts.ts
@@ -14,47 +14,73 @@ export type SearchProps<Filter = string> = {
 export class SearchParams {
     protected _page: number;
     protected _perPage: number = 15;
-    protected _sort: string | null;
+    protected _sortField: string | null;
     protected _sortDir: SortDirection | null;
     protected _filter: string | null;
 
-    constructor(props: SearchProps) {
-        this._page = props.page;
-        this._perPage = props.perPage;
-        this._sort = props.sortField;
-        this._sortDir = props.sortDir;
-        this._filter = props.filter;
+    constructor(props: SearchProps = {}) {
+        this.page = props.page;
+        this.perPage = props.perPage;
+        this.sortField = props.sortField;
+        this.sortDir = props.sortDir;
+        this.filter = props.filter;
     }
 
     get page() {
         return this._page;
     }
 
-    private set page(value: number) { }
+    private set page(value: number) {
+        let _page = +value;
+        if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+            _page = 1;
+        }
+
+        this._page = _page;
+    }
 
     get perPage() {
         return this._perPage;
     }
 
-    private set perPage(value: number) { }
+    private set perPage(value: number) {
+        let _perPage = (value === true as any) ? 0 : +value;
+        if (Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
+            _perPage = this._perPage;
+        };
 
-    get sort() {
-        return this._sort;
+        this._perPage = _perPage;
     }
 
-    private set sort(value: string | null) { }
+    get sortField() {
+        return this._sortField;
+    }
+
+    private set sortField(value: string | null) {
+        this._sortField = (value && !["boolean", "object"].includes(typeof value)) ? `${value}` : null;
+    }
 
     get sortDir() {
         return this._sortDir;
     }
 
-    private set sortDir(value: string | null) { }
+    private set sortDir(value: SortDirection | null) {
+        if (!this._sortField || !value || (["boolean", "object"].includes(typeof value))) {
+            this._sortDir = null;
+            return;
+        };
+
+        const direction = `${value}`.toUpperCase();
+        this._sortDir = !["ASC", "DESC"].includes(direction) ? "ASC" : direction as SortDirection;
+    }
 
     get filter() {
         return this._filter;
     }
 
-    private set filter(value: string | null) { }
+    private set filter(value: string | null) {
+        this._filter = (value && !(["boolean", "object"].includes(typeof value))) ? `${value}` : null;
+    }
 }
 
 export interface ISearchableRepository<
