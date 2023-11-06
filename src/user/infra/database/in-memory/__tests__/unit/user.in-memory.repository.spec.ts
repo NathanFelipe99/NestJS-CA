0f6514cb7608e4404f13c16998793d5bf36f04b0
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-11-06 20:14:37 -0300

    test: testing applySort and applyFilter methods in UserRepositoryInMemory

--- src/user/infra/database/in-memory/__tests__/unit/user.in-memory.repository.spec.ts
+++ src/user/infra/database/in-memory/__tests__/unit/user.in-memory.repository.spec.ts
@@ -26,7 +26,7 @@ describe("Testing UserInMemoryRepository", () => {
             expect(result.toJSON()).toStrictEqual(entity.toJSON());
         });
     });
-    
+
     describe("Testing emailExists method", () => {
         it("Should throw error when email is not found", async () => {
             const email = "test2@gmail.com";
@@ -40,6 +40,63 @@ describe("Testing UserInMemoryRepository", () => {
             expect.assertions(0);
             const email = "test2@gmail.com";
             await SUT.emailExists(email);
+        });
+    });
+
+    describe("Testing applyFilter method", () => {
+        it("Should return all items when filter is null", async () => {
+            const email = "test2@gmail.com";
+            const entity = new UserEntity(UserDataBuilder({ email }));
+            await SUT.insert(entity);
+
+            const result = await SUT.findAll();
+            const spyFilterMethod = jest.spyOn(result, "filter");
+            const filteredItems = await SUT["applyFilter"](result, null);
+            expect(spyFilterMethod).not.toHaveBeenCalled();
+            expect(filteredItems).toStrictEqual(result);
+        });
+
+        it("Should filter name field using filter param", async () => {
+            const items = [
+                new UserEntity(UserDataBuilder({ name: "John Doe" })),
+                new UserEntity(UserDataBuilder({ name: "Doe" })),
+                new UserEntity(UserDataBuilder({ name: "John Wick" }))
+            ];
+
+            const spyFilterMethod = jest.spyOn(items, "filter");
+            const filteredItems = await SUT["applyFilter"](items, "John");
+            expect(spyFilterMethod).toHaveBeenCalled();
+            expect(filteredItems).toHaveLength(2);
+            expect(filteredItems).toStrictEqual([items[0], items[2]]);
+        });
+    });
+
+    describe("Testing applySort method by default (when sort param is null)", () => {
+        it("Should order by createdAt", async () => {
+            const createdAt = new Date();
+            const items = [
+                new UserEntity(UserDataBuilder({ name: "Jane Doe", createdAt })),
+                new UserEntity(UserDataBuilder({ name: "John Doe", createdAt: new Date(createdAt.getTime() + 1) })),
+                new UserEntity(UserDataBuilder({ name: "John Wick", createdAt: new Date(createdAt.getTime() + 2) }))
+            ];
+
+
+            const orderedItems = await SUT["applySort"](items, null, null);
+            expect(orderedItems).toStrictEqual([items[2], items[1], items[0]]);
+        });
+
+        it("Should order by params", async () => {
+            const items = [
+                new UserEntity(UserDataBuilder({ name: "Jane Doe" })),
+                new UserEntity(UserDataBuilder({ name: "Doe John" })),
+                new UserEntity(UserDataBuilder({ name: "John Wick" }))
+            ];
+
+            let orderedItems = await SUT["applySort"](items, "name", "ASC");
+            expect(orderedItems).toStrictEqual([items[1], items[0], items[2]]);
+
+            orderedItems = await SUT["applySort"](items, "name", null);
+            expect(orderedItems).toStrictEqual([items[2], items[0], items[1]]);
         });
     });
 });
