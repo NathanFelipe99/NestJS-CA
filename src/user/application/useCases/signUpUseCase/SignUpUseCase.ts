151e4889428e718ae835b6e3f15e7905f2db0397
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-11-11 01:20:26 -0300

    refact and testing: refactoring signUpUseCase and testing it

--- src/user/application/useCases/signUpUseCase/SignUpUseCase.ts
+++ src/user/application/useCases/signUpUseCase/SignUpUseCase.ts
@@ -1,10 +1,14 @@
 import { BadRequestError } from "@/user/application/errors/BadRequestError";
-import { UserUseCaseTypes } from "../types/user.application.types";
+import { UserUseCaseTypes } from "../../types/user.application.types";
 import { IUserRepository } from "@/user/domain/repositories/user.repository.contracts";
 import { UserEntity } from "@/user/domain/entities/user.entity";
+import { IHashProvider } from "@/shared/application/providers/hash.provider";
 
 export class SignUpUseCase {
-    constructor(private userRepository: IUserRepository.Repository) {}
+    constructor(
+        private userRepository: IUserRepository.Repository,
+        private hashProvider: IHashProvider
+    ) { }
 
     async execute(data: UserUseCaseTypes.SignUpInput): Promise<UserUseCaseTypes.SignUpOutput>{
         const { email, name, password } = data;
@@ -14,7 +18,9 @@ export class SignUpUseCase {
 
         await this.userRepository.emailExists(email);
 
-        const entity = new UserEntity({ email, name, password });
+        const encryptedPassword = await this.hashProvider.generateHash(password);
+        const entity = new UserEntity({ email, name, password: encryptedPassword });
+        
         await this.userRepository.insert(entity);
 
         return entity.toJSON();
---
+++ src/user/application/useCases/signUpUseCase/__tests__/unit/signUp.useCase.spec.ts
@@ -0,0 +1,55 @@
+import { UserInMemoryRepository } from "@/user/infra/database/in-memory/user.in-memory.repository";
+import { SignUpUseCase } from "../../SignUpUseCase";
+import { IHashProvider } from "@/shared/application/providers/hash.provider";
+import { BcryptJsHashProvider } from "@/user/infra/providers/hash/bcryptjs.hash.provider";
+import { UserUseCaseTypes } from "@/user/application/types/user.application.types";
+import { UserDataBuilder } from "@/user/domain/helper/user-data.builder";
+import { BadRequestError } from "@/user/application/errors/BadRequestError";
+import { ConflictError } from "@/shared/domain/errors/ConflictError";
+
+describe("Testing SignUp Use Case", () => {
+    let SUT: SignUpUseCase,
+        repository: UserInMemoryRepository,
+        provider: IHashProvider;
+
+    beforeEach(() => {
+        repository = new UserInMemoryRepository();
+        provider = new BcryptJsHashProvider();
+        SUT = new SignUpUseCase(repository, provider);
+    });
+
+    it("Should create an user", async () => {
+        const spyOnInsert = jest.spyOn(repository, "insert");
+        const userData: UserUseCaseTypes.SignUpInput = UserDataBuilder({});
+
+        const resultEntity = await SUT.execute(userData);
+        expect(spyOnInsert).toHaveBeenCalledTimes(1);
+        expect(resultEntity.id).toBeDefined();
+        expect(resultEntity.createdAt).toBeInstanceOf(Date);
+    });
+
+    it("Shouldn't create an user because email already exists", async () => {
+        const spyOnInsert = jest.spyOn(repository, "insert");
+        const userData: UserUseCaseTypes.SignUpInput = UserDataBuilder({ email: "test@test.com" });
+
+        await SUT.execute(userData);
+
+        expect(spyOnInsert).toHaveBeenCalledTimes(1);
+        await expect(async () => await SUT.execute(userData)).rejects.toBeInstanceOf(ConflictError);
+    });
+
+    it("Should throw error when name isn't providded", async () => {
+        const userData: UserUseCaseTypes.SignUpInput = Object.assign(UserDataBuilder({}), { name: null });
+        await expect(() => SUT.execute(userData)).rejects.toBeInstanceOf(BadRequestError);
+    });
+
+    it("Should throw error when email isn't providded", async () => {
+        const userData: UserUseCaseTypes.SignUpInput = Object.assign(UserDataBuilder({}), { email: null });
+        await expect(() => SUT.execute(userData)).rejects.toBeInstanceOf(BadRequestError);
+    });
+
+    it("Should throw error when password isn't providded", async () => {
+        const userData: UserUseCaseTypes.SignUpInput = Object.assign(UserDataBuilder({}), { password: null });
+        await expect(() => SUT.execute(userData)).rejects.toBeInstanceOf(BadRequestError);
+    });
+});
