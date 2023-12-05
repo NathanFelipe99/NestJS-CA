858e271825c43b27764bca828476095673630b7a
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-12-04 22:51:24 -0300

    config: Prisma configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
 import { UserModule } from './user/infra/user.module';
+import { DatabaseModule } from './shared/infra/database/database.module';
 
 @Module({
-  imports: [EnvConfigModule, UserModule],
+  imports: [EnvConfigModule, UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
