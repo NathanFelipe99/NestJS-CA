1cd1bf4750d7a4042ffa8a7f02bf2b2994329b2f
NathanFelipe99 <nathanfguerlando@gmail.com>
2024-01-07 16:05:31 -0300

    Fix and feat: deleting unnecessary files and preparing environment to end-to-end tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
-import { UserModule } from './user/infra/user.module';
-import { DatabaseModule } from './shared/infra/database/database.module';
+import { Module } from "@nestjs/common";
+import { EnvConfigModule } from "./shared/infra/env-config/env-config.module";
+import { UserModule } from "./user/infra/user.module";
+import { DatabaseModule } from "./shared/infra/database/database.module";
 
 @Module({
   imports: [EnvConfigModule, UserModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
