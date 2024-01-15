99071ebe2bf1a091e35589ca18e5efacfbd84f9e
NathanFelipe99 <nathanfguerlando@gmail.com>
2024-01-15 20:30:16 -0300

    tests: create user method (Controller) e2e tests

---
+++ src/user/infra/__tests__/e2e/create.e2e.spec.ts
@@ -0,0 +1,59 @@
+import { UserRepository } from "@/user/domain/repositories/user.repository.contracts";
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { SignUpDTO } from "../../dtos/signUp.dto";
+import { PrismaClient } from "@prisma/client";
+import setupPrismaTests from "@/shared/infra/database/prisma/testing/setup.prisma.tests";
+import { EnvConfigModule } from "@/shared/infra/env-config/env-config.module";
+import { UserModule } from "../../user.module";
+import { DatabaseModule } from "@/shared/infra/database/database.module";
+import request from "supertest";
+import { UserController } from "../../user.controller";
+import { instanceToPlain } from "class-transformer";
+
+describe("Create end-to-end test", () => {
+    let application: INestApplication,
+        module: TestingModule,
+        repository: UserRepository.Repository,
+        signUpDTO: SignUpDTO;
+
+    const prismaService: PrismaClient = new PrismaClient();
+
+    beforeAll(async () => {
+        setupPrismaTests();
+        module = await Test.createTestingModule({
+            imports: [EnvConfigModule, UserModule, DatabaseModule.forTest(prismaService)]
+        }).compile();
+
+        application = module.createNestApplication();
+        await application.init();
+        repository = module.get<UserRepository.Repository>("UserRepository");
+    }, 12000);
+
+    beforeEach(async () => {
+        await prismaService.user.deleteMany();
+        signUpDTO = {
+            name: "testname",
+            email: "test@mail.com",
+            password: "test123"
+        };
+    });
+
+    describe("POST /user", () => {
+        it("Should create an user", async () => {
+            const endpoint = "/user"
+            const response = await request(application.getHttpServer()).post(endpoint).send(signUpDTO).expect(201);
+            expect(Object.keys(response.body)).toStrictEqual([
+                "id",
+                "name",
+                "email",
+                "createdAt"
+            ]);
+
+            const user = await repository.findById(response.body.id);
+            const userPresenter = UserController.userToResponse(user.toJSON());
+            const serialized = instanceToPlain(userPresenter);
+            expect(response.body).toStrictEqual(serialized);
+        });
+    });
+});
