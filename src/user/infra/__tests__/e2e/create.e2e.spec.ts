ba9b7f71602d0cb918141f579551d8b71f4537a3
NathanFelipe99 <nathanfguerlando@gmail.com>
2024-02-02 00:48:22 -0300

    test: finalising create e2e tests

--- src/user/infra/__tests__/e2e/create.e2e.spec.ts
+++ src/user/infra/__tests__/e2e/create.e2e.spec.ts
@@ -42,15 +42,76 @@ describe("Create end-to-end test", () => {
     });
 
     describe("POST /user", () => {
+        const endpoint = "/user";
         it("Should create an user", async () => {
-            const endpoint = "/user"
             const response = await request(application.getHttpServer()).post(endpoint).send(signUpDTO).expect(201);
-            expect(Object.keys(response.body)).toStrictEqual(["data"]);
+            expect(Object.keys(response["body"])).toStrictEqual(["data"]);
 
-            const user = await repository.findById(response.body.data.id);
+            const user = await repository.findById(response["body"]["data"]["id"]);
             const userPresenter = UserController.userToResponse(user.toJSON());
             const serialized = instanceToPlain(userPresenter);
-            expect(response.body.data).toStrictEqual(serialized);
+            expect(response["body"]["data"]).toStrictEqual(serialized);
+        });
+
+        it("Should return an error with 422 status code when request body is invalid", async () => {
+            const response = await request(application.getHttpServer()).post(endpoint).send({}).expect(422);
+            const { statusCode, message, error } = response["body"];
+            expect(statusCode).toEqual(422);
+            expect(message["length"]).toBeGreaterThan(0);
+            expect(error).toBe("Unprocessable Entity");
+        });
+
+        it("Should return an error with 422 status code when name property is invalid", async () => {
+            delete signUpDTO["name"];
+            const response = await request(application.getHttpServer()).post(endpoint).send(signUpDTO).expect(422);
+
+            const { statusCode, message, error } = response["body"];
+            expect(statusCode).toEqual(422);
+            expect(message).toStrictEqual([
+                "name should not be empty",
+                "name must be a string"
+            ]);
+            expect(error).toBe("Unprocessable Entity");
+        });
+
+        it("Should return an error with 422 status code when email property is invalid", async () => {
+            delete signUpDTO["email"];
+            const response = await request(application.getHttpServer()).post(endpoint).send(signUpDTO).expect(422);
+
+            const { statusCode, message, error } = response["body"];
+            expect(statusCode).toEqual(422);
+            expect(message).toStrictEqual([
+                "email must be an email",
+                "email should not be empty",
+                "email must be a string"
+            ]);
+            expect(error).toBe("Unprocessable Entity");
+        });
+
+        it("Should return an error with 422 status code when password property is invalid", async () => {
+            delete signUpDTO["password"];
+            const response = await request(application.getHttpServer()).post(endpoint).send(signUpDTO).expect(422);
+
+            const { statusCode, message, error } = response["body"];
+            expect(statusCode).toEqual(422);
+            expect(message).toStrictEqual([
+                "password should not be empty",
+                "password must be a string"
+            ]);
+            expect(error).toBe("Unprocessable Entity");
+        });
+
+        it("Should return an error with 422 status code with invalid property provided", async () => {
+            const response = await request(application.getHttpServer()).post(endpoint).send(
+                Object.assign(signUpDTO, { age: 22 })
+            ).expect(422);
+
+            const { statusCode, message, error } = response["body"];
+            expect(statusCode).toEqual(422);
+            expect(message).toStrictEqual([
+                "property age should not exist"
+            ]);
+            expect(error).toBe("Unprocessable Entity");
         });
     });
 });
